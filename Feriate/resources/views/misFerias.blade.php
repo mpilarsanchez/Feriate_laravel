bcd3ab6973389e66f956be602b61add8a29d90f0
M Pilar Sanchez <sanchezmendozpilar@gmail.com>
2019-09-04 12:13:49 -0300

    tabla carrito y otros
    crear tabla carrito en archivo db
    solucionado el problema de las paginas mis ferias y feria cuando no tenían foto 
    link de paginación se saco del div en ferias y mis ferias  y ahora se ve al pie de la pagina

--- Feriate/resources/views/misFerias.blade.php
+++ Feriate/resources/views/misFerias.blade.php
@@ -35,19 +35,19 @@ Mis Ferias
             {{$feria["descripcion"]}}
           </div>
           <div class="cuerpo-feria">
-            @if(!empty($feria["img_nombre"]))
-              <img src="/images/{{ $feria["img_nombre"]}}" alt="">
-              <img src="/images/{{ $feria["img_nombre"]}}" alt="">
+            @if($ImagenFeria == null)
+              <img src="/images/logo_feriate_deffault.png" alt="">
+              <img src="/images/logo_feriate_deffault_ii.png" alt="">
             @else
-               <img src="/images/logo_feriate_deffault.png" alt="">
-               <img src="/images/logo_feriate_deffault_ii.png" alt="">
+              <img src="/storage/{{ $feria->imagen[0]["nombre"]}}" alt="">
+              <img src="/storage/{{ $feria->imagen[0]["nombre"]}}" alt="">
             @endif
           </div>
         </div>
-        {{$datosFerias->links()}}
        @endforeach
    @endif
  </div>
+   {{$datosFerias->links()}}
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
